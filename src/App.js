1f4d8ccd9b0caf4f2f23072cc3c8420302b86c8c
Wadim Skworzow <sempris2008@gmail.com>
2021-12-09 10:02:08 +0100

    test change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>React Deployment</p>
+        <p>React Deployment (Deployed)</p>
       </header>
     </div>
   );
